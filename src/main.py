0f6c2914cf8fc46083ae2183bdf3ba0a5fd1f158
tzsombi01 <toreky.zsombor@gmail.com>
2022-09-17 11:29:23 +0200

    ÃMade changes

--- src/main.py
+++ src/main.py
@@ -1,16 +1,34 @@
 import cv2
 
-COLORS = {"RED" : [0, 0, 255]}
+COLORS = {"RED" : [0, 0, 255], "BLACK": [0, 0, 0]}
 LINE_THICKNESS = 5
+FRAMERATE = 20
 
 face_cascade = cv2.CascadeClassifier(cv2.samples.findFile(
 	"C:/Users/torek/PycharmProjects/Security_Camera/venv/Lib/site-packages/cv2/data/haarcascade_frontalface_default.xml"))
 body_cascade = cv2.CascadeClassifier(cv2.samples.findFile(
 	"C:/Users/torek/PycharmProjects/Security_Camera/venv/Lib/site-packages/cv2/data/haarcascade_fullbody.xml"))
 
+
+"""
+Syntax: cv.VideoWriter(filename, fourcc, fps, frameSize)
+
+Parameters:
+
+filename: Input video file
+fourcc: 4-character code of codec used to compress the frames
+fps: framerate of videostream
+framesize: Height and width of frame
+"""
+
+
+def recordVideo():
+	output = cv2.VideoWriter('filename.avi', cv2.VideoWriter_fourcc(*'MJPG'), 10, size)
+
+
 def detectBodies(frame):
 	grayscale_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-	scale_factor, overlap = 1.3, 5
+	scale_factor, overlap = 1.3, 2
 	bodies = body_cascade.detectMultiScale(grayscale_img, scale_factor, overlap)
 
 	for (body_top_left_x, body_top_left_y, width, height) in bodies:
@@ -28,7 +46,7 @@ def detectFaces(frame):
 	Recomm: 3-6, The lower, the more object gets detected as a face
 	"""
 	grayscale_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-	scale_factor, overlap = 1.3, 5
+	scale_factor, overlap = 1.4, 6
 	faces = face_cascade.detectMultiScale(grayscale_img, scale_factor, overlap)
 
 	for (face_top_left_x, face_top_left_y, width, height) in faces:
@@ -37,10 +55,14 @@ def detectFaces(frame):
 
 
 cap = cv2.VideoCapture(0)
+frame_width = int(cap.get(3))
+frame_height = int(cap.get(4))
+size = (frame_width, frame_height)
 record = True
 while record:
 	_, frame = cap.read()
 
+	# recordVideo()
 	detectFaces(frame)
 	detectBodies(frame)
 
