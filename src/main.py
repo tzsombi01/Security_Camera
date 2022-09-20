1456d8474acca148bcf30f8ccb991c37bcf4291e
tzsombi01 <toreky.zsombor@gmail.com>
2022-09-20 20:32:07 +0200

    Added GeoLocation and Modified Format + Refactoring

--- src/main.py
+++ src/main.py
@@ -4,6 +4,8 @@ import time
 from email.message import EmailMessage
 import smtplib
 import ssl
+import geocoder
+from geopy.geocoders import Nominatim
 
 sender_email = ""
 password = ""
@@ -11,34 +13,43 @@ EMAIL_SENDER_ID = ""
 target_email = ""
 EMAIL_HEADER = "Security Alert"
 EMAIL_SUBJECT = "Security Camera Activated"
-EMAIL_MESSAGE = "The Security Camera Activated at time: "
+EMAIL_MESSAGE_TIME_SUBSTRING = "The Security Camera Activated at time: "
+EMAIL_MESSAGE_LOCATION_SUBSTRING = "The Security Camera Activated at location: "
 
-FRAME_RATE = 20
 SECONDS_TO_RECORD_AFTER_DETECTION = 20
 
 face_cascade = cv2.CascadeClassifier(cv2.samples.findFile(
 	"C:/Users/torek/PycharmProjects/Security_Camera/venv/Lib/site-packages/cv2/data/haarcascade_frontalface_default.xml"))
+body_cascade = cv2.CascadeClassifier(cv2.samples.findFile(
+	"C:/Users/torek/PycharmProjects/Security_Camera/venv/Lib/site-packages/cv2/data/haarcascade_fullbody.xml"))
 
 
 def main():
-	cap = cv2.VideoCapture(0)
-	frame_size = (int(cap.get(3)), int(cap.get(4)))
-	fourCC = cv2.VideoWriter_fourcc(*'mp4v')
+	camera = cv2.VideoCapture(0)
+	FRAME_RATE = int(camera.get(5))
+	frame_size = (int(camera.get(3)), int(camera.get(4)))
+	fourCC = cv2.VideoWriter_fourcc(*"mp4v")
 	detection_stopped_time, timer_started, detecting = (None, False, False)
+	townID, CountyID, CountryID = 4, 6, 10
+	locationStringified = str(getLocation())
+	locationOfCamera = locationStringified.split(",")[townID] + ', ' + \
+					   locationStringified.split(",")[CountyID] + ', ' + \
+					   locationStringified.split(",")[CountryID]
 	running = True
 	while running:
-		_, frame = cap.read()
+		_, frame = camera.read()
 		number_of_faces = detectFaces(frame)
 		# number_of_bodies = detectBodiesHOG(frame)
-		if number_of_faces > 0:
+		# number_of_bodies = detectBodiesCascade(frame)
+		if number_of_faces > 0:  # or number_of_bodies > 0
 			if detecting:
 				timer_started = False
 			else:
 				detecting = True
 				activation_time = getDateAndTimeFormatted()
-				output = cv2.VideoWriter(f'Recordings/{activation_time}.mp4', fourCC, FRAME_RATE, frame_size)
+				output = cv2.VideoWriter(f"Recordings/{activation_time}.mp4", fourCC, FRAME_RATE, frame_size)
 				print(f"Started Recording")
-				sendAnAlertEmail(activation_time)
+				sendAnAlertEmail(activation_time, locationOfCamera)
 		elif detecting:
 			if timer_started:
 				if reachedEndOfRecordingTime(detection_stopped_time):
@@ -56,53 +67,50 @@ def main():
 
 		if cv2.waitKey(1) & 0xFF == ord('q'):
 			running = False
-	try:
-		output.release()
-	except:
-		pass
-	cap.release()
+
+	camera.release()
 	cv2.destroyAllWindows()
 
 
 def detectBodiesHOG(frame):
 	hog = cv2.HOGDescriptor()
 	hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+	grayscale_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	bodies, _ = hog.detectMultiScale(grayscale_img, winStride=(4, 4), padding=(2, 2), scale=1.5)
+	print(f"Length of bodies2 : {len(bodies)}")
+	return len(bodies)
 
-	bodies, _ = hog.detectMultiScale(frame, winStride=(5, 5), padding=(3, 3), scale=1.3)
 
+def detectBodiesCascade(frame):
+	grayscale_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	scale_factor, overlap = 1.3, 4
+	bodies = body_cascade.detectMultiScale(grayscale_img, scale_factor, overlap)
 	return len(bodies)
 
 
 def detectFaces(frame):
-	"""
-	Scale factor: Parameter specifying how much the image size is reduced at each image scale
-	Recomm: 1.1 -> 1.5
-	The lower, the more accurate it is
-	From 1 to 2
-	Last Param: How many overlapping "face" do we need, in order to recognize it as a face
-	Recomm: 3-6, The lower, the more object gets detected as a face
-	"""
 	grayscale_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 	scale_factor, overlap = 1.3, 6
 	faces = face_cascade.detectMultiScale(grayscale_img, scale_factor, overlap)
-
 	return len(faces)
 
 
 def getDateAndTimeFormatted():
-	return datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+	return datetime.now().strftime("%Y/%m/%d %H-%M-%S")
 
 
 def reachedEndOfRecordingTime(detection_stopped_time):
 	return (time.time() - detection_stopped_time) >= SECONDS_TO_RECORD_AFTER_DETECTION
 
 
-def sendAnAlertEmail(timeOfActivation):
+def sendAnAlertEmail(timeOfActivation, location):
 	emailToSend = EmailMessage()
 	emailToSend["From"] = EMAIL_SENDER_ID
+	emailToSend["To"] = target_email
 	emailToSend["Header"] = EMAIL_HEADER
 	emailToSend["Subject"] = EMAIL_SUBJECT
-	emailToSend.set_content(EMAIL_MESSAGE + timeOfActivation)
+	emailMessage = EMAIL_MESSAGE_TIME_SUBSTRING + timeOfActivation + '\n' + EMAIL_MESSAGE_LOCATION_SUBSTRING + location
+	emailToSend.set_content(emailMessage)
 
 	SMTP_SERVER = "smtp.gmail.com"
 	SMTP_PORT = 465
@@ -112,6 +120,14 @@ def sendAnAlertEmail(timeOfActivation):
 		server.login(sender_email, password)
 		server.sendmail(sender_email, target_email, emailToSend.as_string())
 		print(f"Email has been sent to {target_email}")
+
+
+def getLocation():
+	ip = geocoder.ip("me")
+	userLocationCoordinates = (ip.latlng[0], ip.latlng[1])
+	geoLoc = Nominatim(user_agent="_")
+	locationOfUser = geoLoc.reverse(userLocationCoordinates)
+	return locationOfUser
 
 
 if __name__ == "__main__":
